680067c5b6271535062719374ea17c8d220e89f3
shedaniel <daniel@shedaniel.me>
2021-06-13 18:12:42 +0800

    Update MojangMappingsDependency.java

--- src/main/java/net/fabricmc/loom/configuration/providers/mappings/MojangMappingsDependency.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/mappings/MojangMappingsDependency.java
@@ -135,7 +135,7 @@ public class MojangMappingsDependency extends AbstractModuleDependency implement
 			throw new UncheckedIOException(e);
 		}
 
-		Path mappingsFile = mappingsDir.resolve(String.format("mappings-%s.tiny", getVersion()));
+		Path mappingsFile = mappingsDir.resolve(String.format("mojmap-mappigns-%s.jar", getVersion()));
 		Path clientMappings = mappingsDir.resolve("client.map");
 		Path serverMappings = mappingsDir.resolve("server.map");
 
